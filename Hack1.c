cc2f4095a2d3d8b7fb1a51c0df66de84c9847c9b
shpark17 <42521593+shpark17@users.noreply.github.com>
2018-08-23 11:35:11 -0500

    Create Hack1.c
    Hack 1 assignment- SungHa Park

---
+++ Hack1.c
@@ -0,0 +1,16 @@
+/**
+ * Author: SungHa Park
+ * Date: 08/23/2018
+ *
+ * Shows creater's name and major
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+  printf("SungHa Park Electrical engineering\n");
+
+  return 0;
+ }
